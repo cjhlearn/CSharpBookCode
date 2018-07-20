ec602cf3544288cd477b25df1e0e33318d0a3213
cjhlearn <cjhlearn@outlook.com>
2018-07-20 09:30:48 +0800

    first commit

---
+++ 09_code/Delegates-vs2017/DelegatesAndEventsSamples/GetAStringDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+
+namespace GetAStringDemo
+{
+  class Program
+  {
+    //定义一个委托
+    private delegate string GetAString();
+
+    static void Main()
+    {
+      int x = 40;
+      //实例化委托
+      GetAString firstStringMethod = new GetAString(x.ToString);
+      WriteLine($"String is {firstStringMethod()}");
+      // With firstStringMethod initialized to x.ToString(),
+      // the above statement is equivalent to saying
+      // Console.WriteLine("String is {0}", x.ToString());
+
+      var balance = new Currency(34, 50);
+
+      // firstStringMethod references an instance method
+      firstStringMethod = balance.ToString;
+      WriteLine($"String is {firstStringMethod()}");
+
+      // firstStringMethod references a static method
+      firstStringMethod = new GetAString(Currency.GetCurrencyUnit);
+      WriteLine($"String is {firstStringMethod()}");
+    }
+  }
+}
